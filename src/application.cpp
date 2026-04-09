7b09815e8efcaa3f129f42bb03b6de4977352ca5
hamad <hamad@fedora.com>
2026-04-09 18:16:43 +0100

    adjusted cascades

--- src/application.cpp
+++ src/application.cpp
@@ -127,7 +127,7 @@ void application::run()
     camera.position = glm::vec3(5.0f, 5.0f, 5.0f);
     camera.target = glm::vec3(0.0f, 0.0f, 0.0f);
     camera.nearPlane = 0.1f;
-    camera.farPlane = 1000.0f;
+    camera.farPlane = 400.0f;
     camera.fov = 75.0f;
     camera.ratio = (f32)screen_width_ / (f32)screen_height_;
     camera.projection = glm::perspective(glm::radians(camera.fov), camera.ratio, camera.nearPlane, camera.farPlane);
